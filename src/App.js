ab4407cb9b402ba7868284f70c3951b533b74894
Tran Hoang Lan <motden100@gmail.com>
2020-07-31 11:19:57 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import { BrowserRouter, Route, Switch, Redirect, Link } from "react-router-dom";
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import NotFound from './components/NotFound';
+import Header from "./components/Header";
+
+// Lazy load
+const Photo = React.lazy(() => import("./features/Photo"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="photo-app">
+      <Suspense fallback={<div>Loading...</div>}>
+      <BrowserRouter>
+        <Header />
+        {/* <ul>
+          <li><Link to="/photos">Go to photo page</Link></li>
+          <li><Link to="/photos/add">Go to Add photo page</Link></li>
+          <li><Link to="/photos/123">Go to Edit photo page</Link></li>
+        </ul> */}
+
+        <Switch>
+          <Redirect exact from="/" to="/photos"/>
+
+          <Route path="/photos" component={Photo} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+      </Suspense>
     </div>
   );
 }
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import { Container, Row, Col } from "reactstrap";
+import { NavLink } from "react-router-dom";
+import "./Header.scss"
+import { Container, Row, Col } from 'reactstrap';
+const Header = props => {
+    return (
+        <header className="header">
+            <Container>
+                <Row className="justify-content-between">
+                    <Col xs="auto">
+                        <a className="header__link header__title" target="_blank" rel="noopener noreferrer"  href="https://www.freelancer.com/dashboard">Stork</a>
+                    </Col>
+                    <Col xs="auto">
+                        <NavLink className="header__link" to="/photos" activeClassName="header__link--active">Redux Project</NavLink>
+                    </Col>
+                </Row>
+            </Container>
+        </header>
+    );
+};
+
+Header.propTypes = {
+    
+};
+
+export default Header;
